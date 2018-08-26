4e453ac8897ece4c35c4f8465eb005fd45b899b9
akash-akya <akashh246@gmail.com>
2018-08-26 21:45:52 +0530

    Update for heroku

--- main.go
+++ main.go
@@ -1,27 +1,45 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "net/http"
+	"log"
+	"fmt"
+	"time"
+	"net/http"
+	"os"
 )
 
 var comics []XKCDComic
 
+func determineListenAddress() (string, error) {
+  port := os.Getenv("PORT")
+  if port == "" {
+    return "", fmt.Errorf("$PORT not set")
+  }
+  return ":" + port, nil
+}
+
 func main() {
+    addr, err := determineListenAddress()
+    if err != nil {
+	log.Fatal(err)
+    }
+
     loadComics()
 
     go func() {
-        for {
-            crawl()
-            loadComics()
-            time.Sleep(crawlInterval)
-        }
+	for {
+	    crawl()
+	    loadComics()
+	    time.Sleep(crawlInterval)
+	}
     }()
     fmt.Printf("Started background crawl\n");
 
     http.HandleFunc("/", IndexHandler)
     http.HandleFunc("/search", SearchHandler)
 
-    http.ListenAndServe(":8080", nil)
+    // http.ListenAndServe(":8080", nil)
+    if err := http.ListenAndServe(addr, nil); err != nil {
+    panic(err)
+  }
 }
